4feb9404d4c5a040a1f3ab7bdfe8452e808cbe01
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-12-08 12:11:50 +0100

    1

---
+++ pytest_3_selenium/oop1_wstep.py
@@ -0,0 +1,9 @@
+employees = []
+emp1 = ['Monika', 'Kowalska', 23, 43, 'tester']
+emp2 = ['Karol', 'Nowak', 63, 3, 'developer']
+
+employees.append(emp1)
+employees.append(emp2)
+
+print(f'Imie 2go pracowanika to {employees[1][0]}')
+
---
+++ pytest_3_selenium/oop2_przyklad.py
@@ -0,0 +1,10 @@
+class Auto:
+    def __init__(self):
+        self.kolor = 'black'
+        self.ilosc_paliwa = 10
+        self.kondycja = 5
+        self.tryb_ekonomiczny = False
+        self.spalanie_na_100 = 14
+        self.rocznik = 2020
+
+car1 =
