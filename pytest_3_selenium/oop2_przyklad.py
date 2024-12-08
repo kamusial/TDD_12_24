23e79ac71d78c02bf815e2a6768bb60cd50e178e
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-12-08 12:32:39 +0100

    1

--- pytest_3_selenium/oop2_przyklad.py
+++ pytest_3_selenium/oop2_przyklad.py
@@ -1,10 +1,23 @@
 class Auto:
-    def __init__(self):
-        self.kolor = 'black'
+    def __init__(self, kolor, wiek):
+        self.kolor = kolor
         self.ilosc_paliwa = 10
         self.kondycja = 5
         self.tryb_ekonomiczny = False
         self.spalanie_na_100 = 14
-        self.rocznik = 2020
+        self.rocznik = 2024 - wiek
 
-car1 =
+    def zasieg(self):
+        zasieg = self.ilosc_paliwa / self.spalanie_na_100 * 100 * 0.9
+        return round(zasieg)
+
+
+car1 = Auto('red', 12)
+car2 = Auto('blue', 1)
+
+print(car1.rocznik)
+car1.rocznik = 2004
+print(car1.rocznik)
+
+print(car2.ilosc_paliwa)
+print(car2.zasieg())
